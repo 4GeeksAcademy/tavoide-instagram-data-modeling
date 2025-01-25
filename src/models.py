a8f3bceda559e953500a5fae572a1dec319544c4
Tavoide <gustavobenitez116@gmail.com>
2025-01-25 00:07:39 +0000

    modelo de instagram terminado

--- src/models.py
+++ src/models.py
@@ -7,23 +7,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(10), nullable=False)
+    email = Column(String(20), nullable=False) 
+    pasword = Column(String(10), nullable=False )
 
-class Address(Base):
-    __tablename__ = 'address'
+class Seguidores(Base):
+    __tablename__ = 'seguidores'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    me_sigue = Column(Integer, ForeignKey(Usuario.id)) 
+    lo_sigo = Column(Integer, ForeignKey(Usuario.id))
+    person_id = Column(Integer, ForeignKey('Usuario_id'))
+    user = relationship('Usuario')
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column (Integer, primary_key=True)
+    titulo = Column(String(50), nullable=False)
+    contenido = Column(String(100), nullable=False)
+    autor_id = Column(Integer, ForeignKey(Usuario.id), nullable=False)
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+    id = Column (Integer, primary_key=True)
+    contenido = Column (String(250), nullable=False)
+    usuario_id = Column(Integer, ForeignKey(Usuario.id), nullable=False)
+    post_id = Column(Integer, ForeignKey(Post.id), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    tipo= Column(String(50), nullable=False)
+    post_id= Column(Integer, ForeignKey(Post.id))
+
 
     def to_dict(self):
         return {}
